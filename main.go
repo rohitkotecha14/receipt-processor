510cfd6483fda9c1b4e47693cf1c6bbd9ba1c5fe
rohitkotecha14 <rohit.kotecha@sjsu.edu>
2025-02-02 13:50:29 -0800

    Test Commit

--- main.go
+++ main.go
@@ -27,27 +27,3 @@ func loggingMiddleware(next http.Handler) http.Handler {
 		storage.PrintStore()
 	})
 }
-
-//curl -X POST http://localhost:8080/receipts/process \
-//-H "Content-Type: application/json" \
-//-d '{
-//"retailer": "M&M Corner Market",
-//"purchaseDate": "2022-03-20",
-//"purchaseTime": "14:33",
-//"items": [
-//{
-//"shortDescription": "Gatorade",
-//"price": "2.25"
-//},{
-//"shortDescription": "Gatorade",
-//"price": "2.25"
-//},{
-//"shortDescription": "Gatorade",
-//"price": "2.25"
-//},{
-//"shortDescription": "Gatorade",
-//"price": "2.25"
-//}
-//],
-//"total": "9.00"
-//}'
